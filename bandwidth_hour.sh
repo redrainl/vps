477de0dd79c0e53754e0f5cafb899b0f15d39f22
redrainl <redrainl@gmail.com>
2022-03-29 00:48:29 +0800

    bug fix

--- bandwidth_hour.sh
+++ bandwidth_hour.sh
@@ -1,11 +1,20 @@
-aa=$(( $(vnstat -i eth0 -h |tail -1|awk '{print $8}'|sed 's/,//'|awk -F '.' '{print $1}')+$(vnstat -i eth0 -h |tail -1|awk '{print $9}'|sed 's/,//'|awk -F '.' '{print $1}')  ))
-content=$(vnstat -i eth0 -h |awk '{print $7"点：rx"$8,"  tx:"$9}'|tail -5)
-title="Hostdare前一小时双向流量:"$aa"M"
-echo $aa
-echo $content
-echo $title
+aa=$(vnstat -i ens3 -h |tail -2|head|awk '{print $8}')
+unit=$(vnstat -i ens3 -h |tail -2|head|awk '{print $9}')
+if [ $unit == 'GiB' ]; then
+  echo "unit is GiB"
+  aa=$(echo "$aa * 1000"|bc)
+fi
+# echo $unit
+# echo $aa
 
-if  [ $aa -gt 6000 ]; then
+content=$(vnstat -i ens3 -h |awk '{print $1"点：rx"$2$3,"  tx:"$5$6}'|tail -5)
+title="OCP-1前一小时双向流量:"$aa"M"
+
+# echo $content
+# echo $title
+
+if  [ $(echo $aa|awk -F "." '{print $1}') -gt 10000 ]; then
     curl  -o /dev/null --data "token=78443c6feb0241029bfe155c84b90e8d&title=$title&content=$content&template=html" http://pushplus.plus/send
 
 fi
+
