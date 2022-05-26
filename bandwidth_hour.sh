7b2da178817572bd29db1b6193d6208781839981
redrainl <61587864+redrainl@users.noreply.github.com>
2022-05-26 20:45:08 +0800

    Update bandwidth_hour.sh

--- bandwidth_hour.sh
+++ bandwidth_hour.sh
@@ -1,5 +1,6 @@
-aa=$(vnstat -i ens3 -h |tail -2|head|awk '{print $8}')
-unit=$(vnstat -i ens3 -h |tail -2|head|awk '{print $9}')
+bandwidth_limit=5000
+aa=$(vnstat -i eth0 -h |tail -2|head|awk '{print $8}')
+unit=$(vnstat -i eth0 -h |tail -2|head|awk '{print $9}')
 if [ $unit == 'GiB' ]; then
   echo "unit is GiB"
   aa=$(echo "$aa * 1000"|bc)
@@ -7,14 +8,13 @@ fi
 # echo $unit
 # echo $aa
 
-content=$(vnstat -i ens3 -h |awk '{print $1"点：rx"$2$3,"  tx:"$5$6}'|tail -5)
-title="OCP-1前一小时双向流量:"$aa"M"
+content=$(vnstat -i eth0 -h |awk '{print $1"：rx"$2$3,"  tx:"$5$6}'|tail -5)
+title="hostdare前一小时双向流量:"$(echo "scale=2;$aa / 1000"|bc)"G"
 
-# echo $content
-# echo $title
+ echo $content
+ echo $title
 
-if  [ $(echo $aa|awk -F "." '{print $1}') -gt 10000 ]; then
-    curl  -o /dev/null --data "token=78443c6feb0241029bfe155c84b90e8d&title=$title&content=$content&template=html" http://pushplus.plus/send
+if  [ $(echo $aa|awk -F "." '{print $1}') -gt $bandwidth_limit]; then
+    curl  -o /dev/null --data "token=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX&title=$title&content=$content&template=html" http://pushplus.plus/send
 
 fi
-
